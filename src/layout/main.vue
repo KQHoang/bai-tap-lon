d5da6bb794f940e46a48f3713effaef559068ae0
DESKTOP-K9F28LS\Admin <quanghoang8532@gmail.com>
2022-10-22 22:10:31 +0700

    footer

--- src/layout/main.vue
+++ src/layout/main.vue
@@ -3,7 +3,7 @@
     <div class="main-header">
         <div class="header-top">
             <div class="row">
-                <div class="logo col-lg-3 col-md-8">
+                <div class="logo col-sm-7 col-lg-3">
                     <img src="@/assets/images/logo_header.png" alt="">
                 </div>
                 <div class="search col-lg-7 hidden-sm" >
@@ -14,7 +14,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="header-wap-icon col-lg-2 col-md-4 d-flex">
+                <div class="header-wap-icon col-sm-5 col-lg-2 d-flex">
                     <i class="fa fa-user-circle-o fa-xl" aria-hidden="true"></i>
                     <i class="fa fa-search fa-xl" aria-hidden="true"></i>
                     <i class="fa fa-shopping-bag fa-xl" aria-hidden="true"></i>
@@ -57,6 +57,32 @@
             </div>
         </div>
     </div>
+
+    <div class="main-footer w-100">
+        <div class="top-footer">
+            <div class="container-fluid row">
+                <div class="col-sm-12 col-lg-3">
+                    <div class="title-email">
+                        <i class="fa fa-envelope-o" aria-hidden="true"></i>
+                        Đăng ký nhận tin
+                    </div>
+                </div>
+                <div class="col-sm-12 col-lg-5 input-footer">
+                    <input type="text" placeholder="Nhập email của bạn">
+                    <div class="btn-register-email--footer">
+                        ĐĂNG KÝ
+                    </div>
+                </div>
+                <div class="col-sm-12 col-lg-4 hotline-footer">
+                    <i class="fa fa-phone-square fa-xl" aria-hidden="true"></i>
+                    Hỗ trợ / Mua hàng: <span>097 328 58 86</span> 
+                </div>
+            </div>
+        </div>
+        <div class="body-footer">
+
+        </div>
+    </div>
 </div>
 </template>
 
@@ -71,15 +97,19 @@ export default {
 .row{
     align-items: center;
 }
-@media screen and (min-width: 960px){
+@media screen and (min-width: 992px){
     .logo img{
         max-height: 80px !important;
         max-width: 160px !important;
         cursor: pointer;
     }
+    .hotline-footer{
+        padding-left: 30px !important;
+    }
+    
 }
 
-@media screen and (max-width: 1000px){
+@media screen and (max-width: 992px){
     .main-header{
         padding: 0 !important;
     }
@@ -101,6 +131,17 @@ export default {
    }
    .display-sm{
         display: block !important;
+   }
+   .title-email{
+    margin-bottom: 20px !important;
+   }
+   .input-footer{
+    margin-bottom: 10px !important;
+   }
+   .hotline-footer{
+    padding-left: 0;
+    margin-top: 10px !important;
+    margin-bottom: 10px !important;
    }
 }
 
@@ -260,5 +301,70 @@ li:nth-child(2):hover .sub-menu{
 }
 .search-input-bottom input:focus-visible{
     outline: none !important;
+}
+.main-footer{
+    position: fixed;
+    bottom: 0;
+    left: 0;
+}
+.top-footer{
+    background: #f3f3f3;
+    padding: 15px 0;
+}
+.container-fluid{
+    padding-right: 15px;
+    padding-left: 15px;
+    margin-right: auto;
+    margin-left: auto;
+    max-width: 1250px;
+}
+.title-email{
+    text-align: left;
+}
+.title-email .fa{
+    margin-right: 10px;
+}
+.input-footer{
+    position: relative;
+}
+.input-footer input{
+    height: 46px;
+    border: 1px solid #e7e7e7;
+    box-shadow: none;
+    height: 45px;
+    font-weight: 500;
+    padding: 0 20px;
+    background: #fbfbfb;
+    color: #252a2b;
+    width: 100%;
+}
+.input-footer input:focus-visible{
+    outline: none !important;;
+}
+.btn-register-email--footer{
+    position: absolute;
+    top: 0;
+    right: 0;
+    line-height: 45px;
+    right: 0;
+    top: 0;
+    padding: 0 12px;
+    background-color: black;
+    color: #fff;
+    font-size: 12px;
+}
+.hotline-footer{
+    display: flex;
+    align-items: center;
+}
+.hotline-footer .fa{
+    margin-right: 5px;
+}
+
+/* css phần main footer */
+.body-footer{
+    padding: 40px 0 30px;
+    border-bottom: 1px solid #e7e7e7;
+    border-top: 1px solid #e7e7e7;
 }
 </style>
