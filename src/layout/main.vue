b5e6f4eba54816d2a1273fe4c4e8a314346a851f
Trịnh Bắc <trinhgiachang@gmail.com>
2022-11-04 21:14:46 +0700

    4/11/2022

--- src/layout/main.vue
+++ src/layout/main.vue
@@ -57,6 +57,57 @@
             </div>
         </div>
     </div>
+    <div class="content-page">
+        <div class="woocommerce">
+            <div class="u-columns col2-set" id="customer_login">
+                <div class="u-column1 col-1">
+                    <h2><span>Đăng nhập</span></h2>
+                    <form class="woocommerce-form woocommerce-form-login login">
+                        <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+                            <label for="username">Tên tài khoản hoặc địa chỉ email&nbsp;<span class="required">*</span></label>
+                            <input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="username" id="username" autocomplete="username" value>
+                        </p>
+                        <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+                            <label for="password">Mật khẩu&nbsp;<span class="required">*</span></label>
+                            <span class="password-input">
+                                <input type="password" class="woocommerce-Input woocommerce-Input--text input-text" name="password" id="password" autocomplete="current-password" value>
+                                <span class="show-password-input"></span>
+                            </span>
+                            <!-- <span class="show-password-input display-password"></span> -->
+                        </p>
+                        <p class="form-row">
+                            <button type="" class="woocommerce-button button woocommerce-form-login__submit">
+                                Đăng nhập
+                            </button>
+                            <label class="woocommerce-form__label woocommerce-form__label-for-checkbox woocommerce-form-login__rememberme">
+                                <input type="checkbox">
+                                <span>&nbsp;Ghi nhớ mật khẩu</span>
+                            </label>
+                        </p>
+                        <p class="">
+                            <a href="">Quên mật khẩu?</a>
+                        </p>
+                    </form>
+                </div>
+                <div class="u-column2 col-2">
+                    <h2><span>Đăng ký</span></h2>
+                    <form class="woocommerce-form woocommerce-form-register register">
+                        <p class="woocommerce-form-row woocommerce-form-row--wide  form-row form-row-wide">
+                            <label for="reg_email">Địa chỉ Email&nbsp;<span class="required">*</span></label>
+                            <input type="email" class="woocommerce-Input woocommerce-Input--text input-text" name="email" id="reg_email" autocomplete="email" value>
+                        </p>
+                        <p>Một mật khẩu sẽ được gửi đến địa chỉ email của bạn.</p>
+                        <div>
+                            <p>Thông tin cá nhân của bạn sẽ được sử dụng để tăng trải nghiệm sử dụng website, quản lý truy cập vào tài khoản của bạn, và cho các mục đích cụ thể khác được mô tả trong chính sách riêng tư.</p>
+                        </div>
+                        <button type="" class="woocommerce-button button size-14 woocommerce-form-login__submit">
+                                Đăng ký
+                            </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+      </div>
 
     <div class="main-footer w-100">
         <div class="top-footer">
@@ -177,6 +228,254 @@ export default {
 </script>
 
 <style scoped>
+.breadcrumb-shop {
+    background: #f5f5f5;
+}
+.row {
+    margin-right: -15px;
+    margin-left: -15px;
+}
+.woocommerce form.checkout_coupon, .woocommerce form.login, .woocommerce form.register {
+    border: 1px solid #d3ced2;
+    padding: 20px;
+    margin: 2em 0;
+    text-align: left;
+    border-radius: 5px;
+}
+a:hover, a:focus {
+    color: #6c6c70;
+    text-decoration: none;
+    outline: none;
+}
+a {
+    transition: opacity 150ms linear, color 150ms linear, background 150ms linear;
+}
+a {
+    color: #252a2b;
+    text-decoration: none;
+    outline: none;
+    -moz-transition: all 0.2s ease-in-out;
+    -o-transition: all 0.2s ease-in-out;
+    -webkit-transition: all 0.2s ease-in-out;
+    transition: all 0.2s ease-in-out;
+}
+.button {
+    position: relative;
+    display: inline-block;
+    padding: 10px 28px;
+    line-height: normal;
+    border: 1px solid #ffffff;
+    border-radius: 0;
+    text-transform: uppercase;
+    font-size: 12px;
+    text-align: center;
+    letter-spacing: 1px;
+    background-color: transparent;
+    -webkit-transition: color 0.45s cubic-bezier(0.785, 0.135, 0.15, 0.86),border 0.45s cubic-bezier(0.785, 0.135, 0.15, 0.86);
+    z-index: 1;
+    color: #252a2b;
+    overflow: hidden;
+}
+.button:before {
+    position: absolute;
+    content: '';
+    display: block;
+    left: -2px;
+    top: 0;
+    right: -2px;
+    bottom: 0;
+    -webkit-transform: scale(1, 1);
+    transform: scale(1, 1);
+    -webkit-transform-origin: left center;
+    transform-origin: left center;
+    z-index: -1;
+}
+.woocommerce #respond input#submit, .woocommerce a.button, .woocommerce button.button, .woocommerce input.button {
+    font-size: 100%;
+    margin: 0;
+    line-height: 1;
+    cursor: pointer;
+    position: relative;
+    text-decoration: none;
+    overflow: visible;
+    padding: 0.618em 1em;
+    font-weight: 700;
+    border-radius: 3px;
+    left: auto;
+    color: #515151;
+    background-color: #ebe9eb;
+    border: 0;
+    display: inline-block;
+    background-image: none;
+    box-shadow: none;
+    text-shadow: none;
+}
+.woocommerce .woocommerce-form-login .woocommerce-form-login__submit {
+    float: left;
+    margin-right: 1em;
+    font-size: 14px;
+}
+.woocommerce form .form-row {
+    padding: 3px;
+    margin: 0 0 6px;
+}
+.woocommerce form .show-password-input, .woocommerce-page form .show-password-input {
+    position: absolute;
+    right: 0.7em;
+    top: 0.7em;
+    cursor: pointer;
+}
+.woocommerce form .show-password-input.display-password::after, .woocommerce-page form .show-password-input.display-password::after {
+    color: #585858;
+}
+.woocommerce form .show-password-input::after, .woocommerce-page form .show-password-input::after {
+    font-family: WooCommerce;
+    speak: never;
+    font-weight: 400;
+    font-variant: normal;
+    text-transform: none;
+    line-height: 1;
+    -webkit-font-smoothing: antialiased;
+    margin-left: 0.618em;
+    content: "";
+    text-decoration: none;
+}
+.woocommerce form .form-row input.input-text, .woocommerce form .form-row textarea {
+    box-sizing: border-box;
+    width: 100%;
+    margin: 0;
+    outline: 0;
+    line-height: normal;
+}
+.woocommerce form .form-row .input-text, .woocommerce-page form .form-row .input-text {
+    box-sizing: border-box;
+    width: 100%;
+    display: flex;
+}
+#order_comments, .woocommerce input:not([type="radio"]):not([type="checkbox"]), .woocommerce button, .woocommerce select, .woocommerce .button {
+    height: 40px;
+    line-height: 36px;
+    background-color: #f2f2f2;
+    box-shadow: 0 1px 1px rgb(0 0 0 / 13%) inset;
+    font-weight: 400;
+    border: 1px solid #ccc;
+    border-radius: 0;
+    color: #878787;
+    font-size: 13px;
+    max-width: 100%;
+    padding: 0 15px;
+}
+.woocommerce form .show-password-input, .woocommerce-page form .show-password-input {
+    position: absolute;
+    right: 0.7em;
+    top: 0.7em;
+    cursor: pointer;
+}
+input {
+    line-height: normal;
+}
+button, input, optgroup, select, textarea {
+    margin: 0;
+    font: inherit;
+    color: inherit;
+}
+.woocommerce form .form-row {
+    padding: 3px;
+    margin: 0 0 6px;
+}
+.woocommerce form .form-row label {
+    line-height: 2;
+}
+.woocommerce form .form-row label, .woocommerce-page form .form-row label {
+    display: block;
+}
+label {
+    display: inline-block;
+    max-width: 100%;
+    margin-bottom: 5px;
+    font-size: 14px;
+    font-weight: 700;
+}
+.woocommerce form .form-row-wide, .woocommerce-page form .form-row-wide {
+    clear: both;
+}
+p {
+    margin: 0 0 10px 0;
+    line-height: 21px;
+    font-size: 14px;
+}
+.size-14{
+font-size: 14px!important;
+}
+.woocommerce .col2-set, .woocommerce-page .col2-set {
+    width: 100%;
+}
+.breadcrumb-shop .breadcrumb-arrows {
+    padding: 10px 0;
+    background: transparent;
+    border-radius: 0;
+    margin: 0;
+}
+.woocommerce form.checkout_coupon, .woocommerce form.login, .woocommerce form.register {
+    border: 1px solid #d3ced2;
+    padding: 20px;
+    margin: 2em 0;
+    text-align: left;
+    border-radius: 5px;
+}
+h1, h2, h3, h4, h5, h6 {
+    color: #252a2b;
+    margin: 0 0 13px 0;
+    font-weight: 500;
+    line-height: 1.2;
+}
+.woocommerce .col2-set .col-1, .woocommerce-page .col2-set .col-1 {
+    float: left;
+    width: 48%;
+}
+.woocommerce .col2-set .col-2, .woocommerce-page .col2-set .col-2 {
+    float: right;
+    width: 48%;
+}
+.breadcrumb {
+    padding: 8px 15px;
+    margin-bottom: 20px;
+    list-style: none;
+    background-color: #f5f5f5;
+    border-radius: 4px;
+}
+ul, ol {
+    margin-top: 0;
+    margin-bottom: 10px;
+}
+.col-xs-12 {
+    width: 100%;
+}
+.col-xs-1, .col-xs-2, .col-xs-3, .col-xs-4, .col-xs-5, .col-xs-6, .col-xs-7, .col-xs-8, .col-xs-9, .col-xs-10, .col-xs-11, .col-xs-12 {
+    float: left;
+}
+.col-xs-1, .col-sm-1, .col-md-1, .col-lg-1, .col-xs-2, .col-sm-2, .col-md-2, .col-lg-2, .col-xs-3, .col-sm-3, .col-md-3, .col-lg-3, .col-xs-4, .col-sm-4, .col-md-4, .col-lg-4, .col-xs-5, .col-sm-5, .col-md-5, .col-lg-5, .col-xs-6, .col-sm-6, .col-md-6, .col-lg-6, .col-xs-7, .col-sm-7, .col-md-7, .col-lg-7, .col-xs-8, .col-sm-8, .col-md-8, .col-lg-8, .col-xs-9, .col-sm-9, .col-md-9, .col-lg-9, .col-xs-10, .col-sm-10, .col-md-10, .col-lg-10, .col-xs-11, .col-sm-11, .col-md-11, .col-lg-11, .col-xs-12, .col-sm-12, .col-md-12, .col-lg-12 {
+    position: relative;
+    min-height: 1px;
+    padding-right: 15px!important;
+    padding-left: 15px!important;
+}
+@media (min-width: 768px){
+.col-sm-12 {
+    width: 100%;
+}
+.col-sm-1, .col-sm-2, .col-sm-3, .col-sm-4, .col-sm-5, .col-sm-6, .col-sm-7, .col-sm-8, .col-sm-9, .col-sm-10, .col-sm-11, .col-sm-12 {
+    float: left;
+}
+}
+@media (min-width: 1200px){
+.col-lg-12 {
+    width: 100%;
+}
+.col-lg-1, .col-lg-2, .col-lg-3, .col-lg-4, .col-lg-5, .col-lg-6, .col-lg-7, .col-lg-8, .col-lg-9, .col-lg-10, .col-lg-11, .col-lg-12 {
+    float: left;
+}
+}
 .row{
     align-items: center;
 }
